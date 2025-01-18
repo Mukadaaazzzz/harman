be274f90cc9906d20881e579e52007b418ad8bd1
Mukadaaazzzz <tmukadaz18@gmail.com>
2025-01-18 15:33:53 +0100

    imgish

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+
+    
+    images: {
+      unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
